44b0fd33afa32959e6afb3f866e5dc9ff7e475f6
Kudakwashe Taderera <tadererak@gmail.com>
2025-06-01 12:47:44 -0500

    Update views.py

--- backend/users/views.py
+++ backend/users/views.py
@@ -4,6 +4,7 @@ from rest_framework.response import Response
 from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework.parsers import JSONParser
 from rest_framework.permissions import AllowAny
+from rest_framework.views import APIView
 from django.contrib.auth import login
 from .models import User
 from .serializers import (
